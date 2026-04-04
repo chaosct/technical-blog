3c98d1d2fc08a5b65884c286764b829d3ba1e7ce
Carles Julià <carles@protopixel.io>
2026-04-04 12:34:04 +0200

    Add robots.txt, sitemap.xml, and Atom/RSS feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,9 @@ TIMEZONE = "Europe/Madrid"
 DEFAULT_LANG = "ca"
 LOCALE = "ca_ES"
 
+# Plugins
+PLUGINS = ["sitemap"]
+
 # Article configuration
 ARTICLE_PATHS = ["ca", "en"]
 PATH_METADATA = r"(?P<lang>[a-z]{2})/(?P<category>.*)/(?P<slug>.*)\..*"
@@ -26,19 +29,31 @@ PAGE_URL = "pages/{slug}/"
 PAGE_SAVE_AS = "pages/{slug}/index.html"
 
 # Feed generation
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+FEED_ALL_RSS = "feeds/all.rss.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+CATEGORY_FEED_RSS = "feeds/{slug}.rss.xml"
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+# Sitemap
+SITEMAP = {
+    "format": "xml",
+    "priorities": {"articles": 0.8, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "weekly", "pages": "monthly"},
+}
 
 # Theme
 THEME = "notmyidea"
 THEME_TEMPLATES_OVERRIDES = ["theme_overrides"]
 
 # Static paths
-STATIC_PATHS = ["images", "extra/favicon.ico"]
-EXTRA_PATH_METADATA = {"extra/favicon.ico": {"path": "favicon.ico"}}
+STATIC_PATHS = ["images", "extra/favicon.ico", "extra/robots.txt"]
+EXTRA_PATH_METADATA = {
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/robots.txt": {"path": "robots.txt"},
+}
 DEFAULT_SOCIAL_IMAGE = "images/social-card-default.png"
 
 # Markdown extensions
